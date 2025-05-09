9c70c1c9de96fbbe70471735d3c5ec705f4b79f5
Ushakov Roman <symryvvin@gmail.com>
2025-05-10 00:10:17 +0300

    profile edit submit

--- src/scripts/edit_profile.js
+++ src/scripts/edit_profile.js
@@ -1,8 +1,28 @@
-import * as all from './popup.js';
+import * as popup from './popup.js';
 
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileEditPopup = document.querySelector('.popup_type_edit');
 
 profileEditButton.addEventListener('click', (e) => {
-  all.openPopup(profileEditPopup);
+  const formElement = document.querySelector('.popup__form[name=edit-profile');
+
+  const nameInput = profileEditPopup.querySelector('.popup__input_type_name');
+  const jobInput = profileEditPopup.querySelector('.popup__input_type_description');
+
+  const profileTitle = document.querySelector('.profile__title');
+  const profileDescription = document.querySelector('.profile__description');
+
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    profileTitle.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+  }
+
+  formElement.addEventListener('submit', handleFormSubmit);
+
+  popup.openPopup(profileEditPopup);
 });
