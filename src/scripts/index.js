7f40bea8414eae594431873652b72fd9e6c611df
Ushakov Roman <symryvvin@gmail.com>
2025-05-10 21:05:09 +0300

    fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,55 +11,48 @@ initialCards.forEach((card) => {
 
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileEditModal = document.querySelector('.popup_type_edit');
+const profileEditFormElement = document.querySelector('.popup__form[name=edit-profile');
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+
+const profileNameInput = profileEditModal.querySelector('input[name=name');
+const profileDescriptionInput = profileEditModal.querySelector('input[name=description]');
 
 profileEditButton.addEventListener('click', (e) => {
-  const formElement = document.querySelector('.popup__form[name=edit-profile');
-
-  const nameInput = profileEditModal.querySelector('input[name=name');
-  const descriptionInput = profileEditModal.querySelector('input[name=description]');
-
-  const profileTitle = document.querySelector('.profile__title');
-  const profileDescription = document.querySelector('.profile__description');
-
-  nameInput.value = profileTitle.textContent;
-  descriptionInput.value = profileDescription.textContent;
-
-  function handleFormSubmit(e) {
-    e.preventDefault();
-
-    profileTitle.textContent = nameInput.value;
-    profileDescription.textContent = descriptionInput.value;
-  }
-
-  formElement.addEventListener('submit', handleFormSubmit);
+  profileNameInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
 
   modal.openModal(profileEditModal);
 });
 
+profileEditFormElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  profileTitle.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+});
+
 const addCardButton = document.querySelector('.profile__add-button');
 const addCardModal = document.querySelector('.popup_type_new-card');
+const addCardFormElement = document.querySelector('.popup__form[name=new-place');
 
 addCardButton.addEventListener('click', (e) => {
-  const formElement = document.querySelector('.popup__form[name=new-place');
-
-  const placeNameInput = addCardModal.querySelector('input[name=place-name]');
-  const linkInput = addCardModal.querySelector('input[name=link]');
-
-  function handleFormSubmit(e) {
-    e.preventDefault();
-
-    const cardData = {
-      name: placeNameInput.value,
-      link: linkInput.value,
-    };
-
-    cardContainer.prepend(createCard(cardData, cardTemplate));
-
-    modal.closeModal(addCardModal);
-  }
-
-  formElement.addEventListener('submit', handleFormSubmit, { once: true });
-  formElement.reset();
-
   modal.openModal(addCardModal);
+});
+
+const placeNameInput = addCardModal.querySelector('input[name=place-name]');
+const linkInput = addCardModal.querySelector('input[name=link]');
+
+addCardFormElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const cardData = {
+    name: placeNameInput.value,
+    link: linkInput.value,
+  };
+
+  cardContainer.prepend(createCard(cardData, cardTemplate));
+
+  addCardFormElement.reset();
+  modal.closeModal(addCardModal);
 });
