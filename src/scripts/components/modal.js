33bdc7161216027bdf4032735515db52a0690459
Ushakov Roman <symryvvin@gmail.com>
2025-05-12 12:14:13 +0300

    fix close modal by escacpe

--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -1,17 +1,18 @@
 export function openModal(modal) {
   modal.classList.add('popup_is-opened');
 
-  function closeModalEsc(e) {
-    if (e.key === 'Escape') {
-      closeModal(modal, closeModalEsc);
-    }
-  }
-
   document.addEventListener('keydown', closeModalEsc);
 }
 
-export function closeModal(modal, closeModalCallback) {
+function closeModalEsc(e) {
+  if (e.key === 'Escape') {
+    const modal = document.querySelector('.popup_is-opened');
+    closeModal(modal);
+  }
+}
+
+export function closeModal(modal) {
   modal.classList.remove('popup_is-opened');
 
-  document.removeEventListener('keydown', closeModalCallback);
+  document.removeEventListener('keydown', closeModalEsc);
 }
